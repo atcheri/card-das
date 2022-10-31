ee9ee6fbd10b0ac2d77b04e269eca623da64a13f
Atcheri Atch <atcheri@gmail.com>
2022-10-31 16:24:20 +0100

    feat: add omega centauri fonts

--- app/src/components/MainLayout.tsx
+++ app/src/components/MainLayout.tsx
@@ -1,18 +1,18 @@
-import { FC } from "react";
-import { Outlet } from "react-router-dom";
+import { FC } from 'react';
+import { Outlet } from 'react-router-dom';
 
-const container = "min-h-screen flex xl:flex-row flex-col relative";
-const content =
-  "flex flex-1 justify-between bg-siteblack py-8 sm:px-12 px-8 flex-col";
-const logo = "w-[160px] h-[60px] object-contain cursor-pointer";
-const body = "flex-1 flex justify-center flex-col xl:mt-0 my-16";
-const logoPath = "/assets/knight-gundam-bb.png";
+const container = 'min-h-screen flex xl:flex-row flex-col relative';
+const content = 'flex flex-1 justify-between bg-siteblack py-8 sm:px-12 px-8 flex-col';
+const logo = 'w-[160px] h-[60px] object-contain cursor-pointer';
+const body = 'flex-1 flex justify-center flex-col xl:mt-0 my-16';
+const logoPath = '/assets/knight-gundam-bb.png';
 
 const MainLayout: FC = () => {
   return (
     <main className={container}>
       <div className={content}>
         <img src={logoPath} alt="card-das-online-game-logo" className={logo} />
+        <h1 className="font-omega">CARD - DAS</h1>
         <Outlet />
       </div>
     </main>
--- app/tailwind.config.cjs
+++ app/tailwind.config.cjs
@@ -1,10 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{ts,tsx}"],
+  content: ['./index.html', './src/**/*.{ts,tsx}'],
   theme: {
     extend: {
       fontFamily: {
-        gundam: ["Plavsky Condensed", "sans-serif"],
+        gundam: ['Plavsky Condensed', 'sans-serif'],
+        omega: ['omega-centauri-Medium', 'sans-serif'],
       },
     },
   },
