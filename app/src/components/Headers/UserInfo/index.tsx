032659c45562f1d267d04ba0f9111ceac9a12e05
Atcheri Atch <atcheri@gmail.com>
2022-11-03 17:23:22 +0100

    feat: show player name in header

--- app/src/components/Headers/UserInfo/index.tsx
+++ app/src/components/Headers/UserInfo/index.tsx
@@ -1,9 +1,13 @@
-import { FC, useEffect } from 'react';
+import { FC } from 'react';
 import Jazzicon, { jsNumberForAddress } from 'react-jazzicon';
+
+import useEthContext from '../../../hooks/useEthContext';
 
 type UserInfoProps = { address: string };
 
 const UserInfo: FC<UserInfoProps> = ({ address }) => {
+  const { player } = useEthContext();
+
   if (!address) {
     return null;
   }
@@ -11,7 +15,7 @@ const UserInfo: FC<UserInfoProps> = ({ address }) => {
   return (
     <div className="flex gap-4 items-center">
       <Jazzicon diameter={28} seed={jsNumberForAddress(address)} />
-      <span className="font-omega">{address.slice(0, 5)}</span>
+      <span className="font-omega">{player ? player.name : address.slice(0, 5)}</span>
     </div>
   );
 };
