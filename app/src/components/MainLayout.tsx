616abe7a0db994653bb94d3b1fb99170c3132820
Atcheri Atch <atcheri@gmail.com>
2022-11-03 23:26:51 +0100

    feat: add a landing page hero image

--- app/src/components/MainLayout.tsx
+++ app/src/components/MainLayout.tsx
@@ -7,21 +7,31 @@ import MainHeader from './Headers/MainHeader';
 
 import 'react-toastify/dist/ReactToastify.css';
 
-const container = 'min-h-screen flex flex-col relative bg-siteblack';
+const mainContainer = 'min-h-screen flex flex-col relative bg-siteblack';
+const container = 'min-h-screen flex flex-col xl:flex-row relative bg-siteblack';
 const content = 'flex flex-1 justify-between flex-col px-4 sm:px-12 sm:py-6 ';
 const body = 'flex flex-1 justify-center flex-col space-y-10 xl:mt-0 my-16';
-
+const LandingBackground = '/assets/landing-background.webp';
 const MainLayout: FC = () => {
   return (
     <>
       <ToastContainer />
-      <div className={container}>
-        <main className={content}>
-          <MainHeader />
-          <div className={body}>
-            <Outlet />
+      <div className={mainContainer}>
+        <div className={container}>
+          <main className={content}>
+            <MainHeader />
+            <div className={body}>
+              <Outlet />
+            </div>
+          </main>
+          <div className="flex flex-1">
+            <img
+              src={LandingBackground}
+              alt="card-das-background"
+              className="w-full xl:h-full object-cover filter brightness-[45%]"
+            />
           </div>
-        </main>
+        </div>
         <Footer />
       </div>
     </>
